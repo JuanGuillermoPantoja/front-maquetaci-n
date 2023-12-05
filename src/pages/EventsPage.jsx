f3c785cc5474147f6d24428fbb9fcb97b486171a
JuanGuillermoPantoja <jgpantoja13@gmail.com>
2023-12-05 16:34:33 -0500

    add-image eventPage

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import notification from '../img/notificacion.png'
 import Navbar from '../components/Navbar'
+import eventImage from '../img/eventos-sociales.jpg'
 
 
 function EventsPage() {
@@ -12,7 +13,7 @@ function EventsPage() {
         <div className='w-full flex h-full justify-center items-center gap-3 flex-wrap'>
        
               <div className='bg-[#000000a4] w-1/4 h-[90%] flex flex-col justify-between rounded-2xl text-[#FFEEB3]' >
-                <img className="h-2/4 w-[90%] border-4 m-4 rounded-2xl border-[#AC703E]" src="" alt="" />
+                <img className="h-2/4 w-[90%] border-4 m-4 rounded-2xl border-[#AC703E]" src={eventImage} alt="" />
                 <h2 className='text-center text-2xl'>nombre evento</h2>
                 <p className='text-center text-xl'>Dirección del evento</p>
                 <p className='text-center text-xl h-18 w-3/4 mx-auto'>descripción del evento</p>
@@ -22,9 +23,7 @@ function EventsPage() {
                   <button className='bg-[#FFEEB3] text-[#AC703E] text-xl m-2 font-bold h-12 w-1/3 rounded-full mt-2 hover:bg-red-700 hover:text-[#FFEEB3] duration-300'>Eliminar</button>
                   <img className='h-12 m-2' src={notification} alt="" />
                 </div>
-              </div>
-            ))
-          
+              </div>   
         </div>
       </div>
     </>
